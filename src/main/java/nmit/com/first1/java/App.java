bd682829fb6c9753162a4ac43875c747edd73eb1
shivaji4890 <shivajigundabattina123@gmail.com>
2023-11-24 14:33:04 +0530

    java

---
+++ src/main/java/nmit/com/first1/java/App.java
@@ -0,0 +1,13 @@
+package nmit.com.first1.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/nmit/com/first1/java/AppTest.java
@@ -0,0 +1,20 @@
+package nmit.com.first1.java;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
